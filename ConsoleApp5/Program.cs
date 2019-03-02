dbbdadc4ee5e0ff1e14b514738ba5250591caa17
Atwoa2 <47752525+Atwoa2@users.noreply.github.com>
2019-03-02 12:40:21 +0200

    Add files via upload

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -7,39 +7,59 @@ using System.Threading.Tasks;
 namespace ConsoleApp3
 {
     class Program
+
     {
-        static void Main(string[] args)
+        Double a, b, c;
+
+        public static IAppDomainSetup (Double[] args)
         {
-            Console.WriteLine("Пример 1");
+
             Console.WriteLine("Ввыедите число a");
-            string x = Console.ReadLine();
-            Double a = Convert.ToDouble(x);
+            //string x = Console.ReadLine();
+            a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Ввыедите число b");
-            string y = Console.ReadLine();
-            Double b = Convert.ToDouble(y);
+            //string y = Console.ReadLine();
+            b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Ввыедите число c");
-            string z = Console.ReadLine();
-            Double c = Convert.ToDouble(z);
+            //string z = Console.ReadLine();
+            c = Convert.ToDouble(Console.ReadLine());
 
-            Double d = (((b + (Math.Sqrt(b * b + 4 * a * c)) / (2 * a))) - (a * a * a * c + Math.Pow(b, -2)));
-            Console.WriteLine("Ответ 1:");
-            Console.WriteLine(d);
-            Console.WriteLine("Пример 2");
-
-            Double f = ((a / c) * (b / d)) - ((a * b - c) / (c * d));
-            Console.WriteLine("Ответ 2:");
-            Console.WriteLine(f);
-
-            Console.WriteLine("Пример 3");
-
-            Double e = ((Math.Sin(a) + Math.Cos(b)) / (Math.Cos(a) - Math.Sin(b))) * Math.Tan(a * b);
-
-            Console.WriteLine("Ответ 2:");
-            Console.WriteLine(e);
-
-            Console.ReadLine();
+            task1()
+            task2()
+            //task3()
+            //task4()
+            //task5()
+            //task6()
 
 
+        }
+
+        public static void task1()
+        {
+            if (a==0)
+            {
+                Console.WriteLine("Решение невозможно");
+            }
+            else
+            {
+                Double d = (((b + (Math.Sqrt(b * b + 4 * a * c)) / (2 * a))) - (a * a * a * c + Math.Pow(b, -2)));
+                Console.WriteLine("Ответ 1:");
+                Console.WriteLine(d);
+            }
+
+        }
+        public static void task2()
+        {
+           Double f = ((a / c) * (b / d)) - ((a * b - c) / (c * d));
+           Console.WriteLine("Ответ 2:");
+           Console.WriteLine(f);
+           Console.WriteLine("Пример 3");
+           
+           Double e = c!=0 ,d != 0 ? (((Math.Sin(a) + Math.Cos(b)) / (Math.Cos(a) - Math.Sin(b))) * Math.Tan(a * b)):"нет решения"  ;
+           Console.WriteLine("Ответ 2:");
+            
+           Console.ReadLine();
+           
 
         }
     }
